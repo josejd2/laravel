27c1025dff84ff45138f131fa2e16ffebd654b5e
josejd2 <josejd2.jlv@gmail.com>
2025-10-02 22:07:33 +0200

    Creo un index y que diriga a products

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,5 +7,22 @@
 </head>
 <body>
     <h1>Listado de productos</h1>
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Precio</th>
+            <th>Descripcion</th>
+        </tr>
+        @foreach ($products as $product)
+        <tr>
+            <td>{{ $product->id }}</td>
+            <td>{{ $product->name }}</td>
+            <td>{{ $product->price }}</td>
+            <td>{{ $product->description }}</td>
+        </tr>
+        @endforeach
+    </table>
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,12 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource(('products'), ProductController::class);
+Route::get('/products', function () {
+    return view('products.index');
+});
+
+Route::resource('products', ProductController::class);
